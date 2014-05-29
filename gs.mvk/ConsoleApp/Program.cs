6906def6a79471388b538a83b94e2b8dc8ee6606
tarurar <tarutin_andrew@mail.ru>
2014-05-29 15:32:48 -0700

    5 content types ready

--- gs.mvk/ConsoleApp/Program.cs
+++ gs.mvk/ConsoleApp/Program.cs
@@ -73,6 +73,7 @@ namespace ConsoleApp
                     deleteListsFromContentType(web, "0x01009435E8C0C3E94107BD6F56DE758815D6");
                     deleteListsFromContentType(web, "0x01009E8BDC3C1146476781B2724822295453");
                     deleteListsFromContentType(web, "0x0100A3A87A1CEE124BB98A5563B3F2D5486C");
+                    deleteListsFromContentType(web, "0x0100ED5F7CB6A1DD4E8D9D5163520BDDD6D0");
                     
                     /*SPContentType ct = web.ContentTypes[new SPContentTypeId("0x0100C274D4A47E4742EEABF2D5FDF35DFBC9")];
                     IEnumerable<SPContentTypeUsage> u = SPContentTypeUsage.GetUsages(ct);
---
+++ gs.mvk/ContentTypes/Features/gs_mvk_ReportLists/gs_mvk_ReportLists.EventReceiver.cs
@@ -0,0 +1,150 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using gs.mvk.BusinessLogic.Utils;
+
+namespace gs.mvk.ContentTypes.Features.gs_mvk_ReportLists
+{
+    /// <summary>
+    /// This class handles events raised during feature activation, deactivation, installation, uninstallation, and upgrade.
+    /// </summary>
+    /// <remarks>
+    /// The GUID attached to this class may be used during packaging and should not be modified.
+    /// </remarks>
+
+    [Guid("d04a7f88-10cd-4b5a-81f4-312c59c08b90")]
+    public class gs_mvk_ReportListsEventReceiver : SPFeatureReceiver
+    {
+        #region Constants
+
+        private const string resFileRelative        = @"gs.mvk\AssignmentListsStrings";
+        private const string resDispNameToken       = "$Resources:AssignmentDependentAssignmentDispName";
+        private const string resReportDispNameToken = "$Resources:AssignmentLastReportMVKDispName";
+        private const string resDescriptionToken    = "$Resources:AssignmentDependentAssignmentDescr";
+        private const string resReportDescrToken    = "$Resources:AssignmentLastReportMVKDescr";
+        private const string resColGroupToken       = "$Resources:MVKColumnsGroup";
+        private const string resFmtListNotFound     = "$Resources:ListNotFoundFmtErr";
+        private const string resFmtColumnAddFailed  = "$Resources:SiteColumnAddFmtErr";
+        private const string resFmtCTNotFound       = "$Resources:ContentTypeNotFoundFmtErr";
+
+        private const string ReportListRelativeUrl = "/Lists/ReportMVKList";
+
+        #endregion
+
+        private SPList GetLookupList(SPWeb web)
+        {
+            SPList retVal = null;
+
+            try
+            {
+                retVal = web.GetList(ReportListRelativeUrl);
+            }
+            catch (FileNotFoundException)
+            {
+                var msgFmt = SPUtility.GetLocalizedString(resFmtListNotFound, resFileRelative, web.Language);
+                System.Diagnostics.Debug.WriteLine(String.Format(msgFmt, ReportListRelativeUrl, web.Title));
+            }
+
+            return retVal;
+        }
+
+        private SPField AddReportLinkColumn(SPWeb web, SPList lookupList, string groupName, string displayName, string description)
+        {
+            string fieldXml = "<Field " +
+                                        "Group=\"" + groupName + "\" " +
+                                        "DisplayName=\"" + displayName + "\" " +
+                                        "ID=\"{0EC1DABE-3F49-4FBA-BA0C-6C11779C758B}\" " +
+                                        "SourceID=\"http://schemas.microsoft.com/sharepoint/v3\" " +
+                                        "StaticName=\"AssignmentLastReportMVK\" " +
+                                        "Name=\"AssignmentLastReportMVK\" " +
+                                        "Type=\"Lookup\" " +
+                                        "List=\"{" + lookupList.ID + "}\" " +
+                                        "ShowField=\"ID\" " +
+                                        "Mult=\"False\" " +
+                                        "UnlimitedLengthInDocumentLibrary=\"False\" " +
+                                        "Description=\"" + description + "\" " +
+                                        "Overwrite=\"TRUE\" " +
+                                        "AllowDeletion=\"FALSE\"></Field>";
+            SPField retVal = Utility.EnsureSiteColumnXml(web, displayName, fieldXml);
+            if (retVal == null)
+            {
+                var msgFmt = SPUtility.GetLocalizedString(resFmtColumnAddFailed, resFileRelative, web.Language);
+                System.Diagnostics.Debug.WriteLine(String.Format(msgFmt, "AssignmentLastReportMVK"));
+            }
+
+            return retVal;
+        }
+
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            SPSite site = (SPSite)properties.Feature.Parent;
+            if (site == null)
+                throw new Exception("Feature must be activated at site collection level");
+
+            foreach (SPWeb web in site.AllWebs)
+            {
+                var gName = SPUtility.GetLocalizedString(resColGroupToken, resFileRelative, web.Language);
+                var cName = SPUtility.GetLocalizedString(resReportDispNameToken, resFileRelative, web.Language);
+                var dName = SPUtility.GetLocalizedString(resReportDescrToken, resFileRelative, web.Language);
+
+                SPList lookupList = GetLookupList(web);
+                if (lookupList != null)
+                {
+                    SPField field = AddReportLinkColumn(web, lookupList, gName, cName, dName);
+                    if (field != null)
+                    {
+                        SPContentType ct = web.ContentTypes["AssignmentMVK"];
+                        if (ct != null)
+                        {
+                            Utility.LinkFieldToContentType(web, ct, field);
+                        }
+                        else
+                        {
+                            var msgFmt = SPUtility.GetLocalizedString(resFmtCTNotFound, resFileRelative, web.Language);
+                            System.Diagnostics.Debug.WriteLine(String.Format(msgFmt, "AssignmentMVK"));
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // Uncomment the method below to handle the event raised before a feature is deactivated.
+
+        public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        {
+            SPSite site = (SPSite)properties.Feature.Parent;
+            if (site == null)
+                throw new Exception("Feature must be activated at site collection level");
+
+            foreach (SPWeb web in site.AllWebs)
+            {
+                var cName = SPUtility.GetLocalizedString(resReportDispNameToken, resFileRelative, web.Language);
+                Utility.RemoveSiteColumn(web, cName);
+            }
+        }
+
+
+        // Uncomment the method below to handle the event raised after a feature has been installed.
+
+        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised before a feature is uninstalled.
+
+        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+        // Uncomment the method below to handle the event raised when a feature is upgrading.
+
+        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        //{
+        //}
+    }
+}
